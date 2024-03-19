cfd6f8ba15dd1e99cfa05f3353a07ee27eb191b9
jash7771 <jashuva.peyyala97@gmail.com>
2024-03-19 16:13:27 +0530

    auth key added

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ from bson import ObjectId
 import json
 import os
 from urllib.parse import quote_plus
+from functools import wraps
+from dotenv import load_dotenv
 
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = b'ESANHKD8976DS8DSA$^*&^*&BOH9YWSDF#'
@@ -18,19 +23,54 @@ socketio = SocketIO(app)
 
 logging.basicConfig(filename='All_log.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
+AUTH_KEY = os.getenv("AUTH_KEY")
 
+# Update the index route to include authentication check
 @app.route('/')
 def index():
     try:
+        # Check if user is authenticated
+        if 'authenticated' not in session:
+            return redirect(url_for('login'))  # Redirect to login page if not authenticated
+        
         agent_ips, agent_names = get_agent_ips_and_names() 
         agent_options = ['{} - {}'.format(ip, name) for ip, name in zip(agent_ips, agent_names)]
         return render_template('index.html', agent_options=agent_options, predefined_rules=predefined_rules_list)
     except Exception as e:
         logger.error("An error occurred: %s", str(e))
         return "An error occurred while loading the page."
-    
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        auth_key = request.form['auth_key']
+        # Replace 'YOUR_AUTH_KEY' with your actual authentication key/token
+        if auth_key == AUTH_KEY:
+            session['authenticated'] = True
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid authentication key'
+    return render_template('login.html')
+# Logout route (optional)
+@app.route('/logout')
+def logout():
+    session.pop('authenticated', None)
+    return redirect(url_for('login'))
+
+
+# Decorator function to require authentication
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'authenticated' not in session:
+            return redirect(url_for('login'))  # Redirect to login page if not authenticated
+        return f(*args, **kwargs)
+    return decorated_function
+
 # agent selection function
 @app.route('/select_agent', methods=['POST'])
+@login_required
 def select_agent():
     try:
         selected_value = request.form.get('agent_ip')
@@ -53,6 +93,7 @@ def select_agent():
 
 #inbound rule function
 @app.route('/inbound-rules', methods=['POST'])
+@login_required
 def apply_inbound_rule():
     try:
         inbound_rule = request.json.get('inbound_rule')
@@ -83,6 +124,7 @@ def apply_inbound_rule():
     
 #OutBound Function
 @app.route('/outbound-rules', methods=['POST'])
+@login_required
 def apply_outbound_rule():
     try:
         outbound_rule = request.json.get('outbound_rule')
@@ -113,6 +155,7 @@ def apply_outbound_rule():
     
 #Bloack Port separate function
 @app.route('/block_port', methods=['GET', 'POST'])
+@login_required
 def block_port():
     if request.method == 'POST':
         try:
@@ -134,12 +177,14 @@ def block_port():
         return render_template('block_port.html')
 
 @app.route('/add_agent')
+@login_required
 def add_ageents():
     agents = agents_collections.find()
     return render_template('add_agent.html', agents=agents)
 
 # add agent function
 @app.route('/add', methods=['POST','GET'])
+@login_required
 def add_agent():
     try:
         agent_id = request.form.get('agent_id')
@@ -158,17 +203,19 @@ def add_agent():
         return str(e), 400
 
 @app.errorhandler(Exception)
+@login_required
 def handle_error(e):
     logger.error("An unexpected error occurred: %s", str(e))
     return jsonify({'error': 'An unexpected error occurred'}), 500
 
-
 @app.route('/rule_manager',methods=['POST','GET'])
+@login_required
 def rule_manager():
     return render_template('manage_rules.html')
 
 #agent downloader 
 @app.route('/download_agent',methods=['POST','GET'])
+@login_required
 def download_agent():
     return  render_template("download_agent.html")
 
@@ -189,6 +236,7 @@ def download_agent():
 
 #flush rules function
 @app.route('/flush_rules', methods=['GET','POST'])
+@login_required
 def flush_rules():
     if 'AGENT_URL' in session:
         try:
@@ -201,6 +249,7 @@ def flush_rules():
 
 #show rules function
 @app.route('/show_rules', methods=['GET','POST'])
+@login_required
 def show_rules():
     if 'AGENT_URL' in session:
         try:
--- rules.py
+++ rules.py
@@ -1,7 +1,19 @@
 import pymongo
+from pymongo import MongoClient
 
 # MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+from urllib.parse import quote_plus
+
+username = "alpha"
+password = "Alpha#Jash@777"
+encoded_username = quote_plus(username)
+encoded_password = quote_plus(password)
+
+client = MongoClient(f"mongodb+srv://{encoded_username}:{encoded_password}@dashboard.wnz5mjp.mongodb.net/") 
+
+
 db = client["iptables_rules"]
 collection = db["rules"]
 inbound_rules_collection = db['inbound_rules']
